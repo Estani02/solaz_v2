f7d715c707b21247e1f3ed1240cb75dd61c2709d
Estani02 <estaniolmedo@gmail.com>
2023-09-27 11:32:39 -0300

    fix gtag

--- src/lib/gtagHelper.ts
+++ src/lib/gtagHelper.ts
@@ -1,3 +1,10 @@
+declare global {
+  interface Window {
+    gtag: (event: string, GA_MEASUREMENT_ID: string, options?: Record<string, any>) => void
+  }
+}
+
+// Luego puedes usar la función 'pageview'
 export const pageview = (GA_MEASUREMENT_ID: string, url: string) => {
   window.gtag('config', GA_MEASUREMENT_ID, {
     page_path: url,
